06260b2898d28f0df226f6dfa3386f654c756cc4
acz19 <acz1992@hotmail.co.uk>
2024-01-17 13:01:37 +0100

    Set up GEO API and fix 403 result

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,8 +1,40 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../../api";
 
 export const Search = ({ onSearchChange }) => {
 	const [search, setSearch] = useState(null);
+
+	/* const loadOptions = async (inputValue) => {
+		try {
+			const response = await fetch(GEO_API_URL, geoApiOptions);
+			const result = await response.text();
+			console.log(result);
+		} catch (error) {
+			console.error(error);
+		}
+	}; */
+
+	const loadOptions = async (searchInputValue) => {
+		try {
+			const response = await fetch(
+				`${GEO_API_URL}?minPopulation=1000000&namePrefix=${searchInputValue}`,
+				geoApiOptions
+			);
+			const result = await response.json();
+
+			return {
+				options: result.data.map((city) => {
+					return {
+						value: `${city.latitude} ${city.longitude}`,
+						label: `${city.name}, ${city.countryCode}`,
+					};
+				}),
+			};
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
 	const handleOnChange = (searchData) => {
 		setSearch(searchData);
@@ -12,9 +44,10 @@ export const Search = ({ onSearchChange }) => {
 	return (
 		<AsyncPaginate
 			placeholder="Search for city"
-			debounceTimeout={600}
+			debounceTimeout={1000}
 			value={search}
 			onChange={handleOnChange}
+			loadOptions={loadOptions}
 		/>
 	);
 };
